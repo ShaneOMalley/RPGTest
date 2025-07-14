de24cade0154266da802bdbfa7e981c9b5d216e5
ShaneOMalley <shane.omalley2@gmail.com>
2025-07-14 21:52:08 +0100

    add unsaved file

--- scripts/game/battle/fsm/state_battle_turn_decision_enemy.gd
+++ scripts/game/battle/fsm/state_battle_turn_decision_enemy.gd
@@ -7,7 +7,7 @@ func _make_attack() -> BattleAbility:
 	return attack
 
 func update(_delta: float) -> void:
-	var attack = _make_attack()
+	# var attack = _make_attack()
 	# BattleManager.queue_ability(attack)
 	var source = BattleManager.get_current_turn().participant
 	BattleManager.queue_ability(BattleAbilityPass.new(source, null))
