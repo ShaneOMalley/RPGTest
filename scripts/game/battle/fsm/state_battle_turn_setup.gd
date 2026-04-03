dcde19d661f2a769226d7caa44f82016cb83d261
ShaneOMalley <shane.omalley2@gmail.com>
2026-04-03 18:17:58 +0900

    Improve turn shrinking ui effect

--- scripts/game/battle/fsm/state_battle_turn_setup.gd
+++ scripts/game/battle/fsm/state_battle_turn_setup.gd
@@ -2,4 +2,15 @@ class_name StateBattleTurnSetup extends FSMState
 
 # go to next turn from list in BattleManager
 func on_enter() -> void:
-	BattleManager.goto_next_turn()
+	var current_turn := BattleManager.get_current_turn()
+	if is_instance_valid(current_turn):
+		var turn_manipulation := BattleTurn.TurnManipulation.new()
+		turn_manipulation.turns = [current_turn]
+		turn_manipulation.anim_name = &"shrink"
+		turn_manipulation.type = BattleTurn.TurnManipulation.Type.REMOVE
+		BattleManager.on_battle_turn_manipulation.emit([turn_manipulation])
+		
+		set_timer(0.4, BattleManager.goto_next_turn)
+		BattleManager.block_fsm(0.41)
+	else:
+		BattleManager.goto_next_turn()
